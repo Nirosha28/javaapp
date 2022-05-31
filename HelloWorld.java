3e4629df77b73a419a1a50a6ca1788d2e51b1173
Nirosha28 <100923419+Nirosha28@users.noreply.github.com>
2022-05-31 19:27:53 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world ,happy holi");
+    System.out.println("Hello world ,happy holi,good morning");
   
 }
 }
